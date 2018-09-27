b7aaa93c20a302ac733b552f1efc53f1b4db8ce4
Catherine Vasquez <cathvasquez03@gmail.com>
2018-09-27 16:47:39 -0700

    My project setup completed successfully

---
+++ projectshoppingbackend/src/main/java/net/ccv/projectshoppingbackend/App.java
@@ -0,0 +1,13 @@
+package net.ccv.projectshoppingbackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
